9addd5256a2d563e0de7b0c065c295527b1cffd3
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2024-02-07 00:55:48 +0000

    Updated server file

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const inventoryRoutes = require("./inventory/routes");
+
 const app = express();
 const port = 3000;
 
@@ -12,6 +14,11 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 }
 );
+
+// API Route
+// http://localhost:3000/inventory
+app.use("/api/v1/inventory", inventoryRoutes);
+
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
